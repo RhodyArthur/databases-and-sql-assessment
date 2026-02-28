d46e35246984d504260138a4682d22bd3eb33419
RhodyArthur <rhodaarthur48@gmail.com>
2026-02-28 20:43:43 +0000

    feat: update SQL queries for accuracy and completeness

--- queries.sql
+++ queries.sql
@@ -16,7 +16,7 @@ FROM books
 -- Get all members who joined in the last 6 months
 SELECT *
 FROM members
-WHERE join_date < CURRENT_DATE - INTERVAL '6 months'
+WHERE join_date >= CURRENT_DATE - INTERVAL '6 months'
 
 
 -- Get all books where available_copies > 0, ordered by title
@@ -27,27 +27,76 @@ ORDER BY title
 
 
 -- Get unique genres from the books table
-SELECT UNIQUE genre
+SELECT DISTINCT genre
 FROM books
 
 
 -- Get the top 5 most expensive books
-SELECT price
+SELECT title, price
 FROM books
 ORDER BY price desc 
 LIMIT 5
 
 
-
-
 -- Task 3.2: WHERE Clause and Filtering
--- Your code here
+-- Get all books published between 2010 and 2020
+SELECT *
+FROM books
+WHERE publication_year BETWEEN 2010 AND 2020
 
+
+-- Get all members with 'gmail.com' email addresses
+SELECT *
+FROM members
+WHERE email LIKE '%@gmail.com'
+
+
+-- Get all books that cost between $10 and $30
+SELECT *
+FROM books
+WHERE price BETWEEN 10 AND 30
+
+
+-- Get all borrowings that are currently overdue (status='overdue' OR due_date < current_date)
+SELECT *
+FROM borrowings
+WHERE status = 'overdue' OR due_date < CURRENT_DATE
+
+
+-- Get all books whose title contains the word 'Python' (case-insensitive)
+-- Your SQL here
+SELECT *
+FROM books
+WHERE title ILIKE '%python%'
 
 
 
 -- Task 3.3: JOINs
--- Your code here
+-- INNER JOIN: Get all books with their author names
+SELECT b.title, a.name
+FROM books b
+INNER JOIN authors a
+WHERE b.author_id = a.id
+
+
+-- LEFT JOIN: Get all authors and count of their books (include authors with 0 books)
+SELECT a.name, COUNT(b.author_id)
+FROM authors a
+LEFT JOIN books b
+WHERE a.id = b.author_id
+GROUP BY a.author_id
+
+
+-- Get all borrowings with member name and book title
+-- Your SQL here
+
+
+-- Get all members who have never borrowed a book (use LEFT JOIN and WHERE)
+-- Your SQL here
+
+
+-- Get books with their authors and genres (multiple joins)
+-- Your SQL here
 
 
 
